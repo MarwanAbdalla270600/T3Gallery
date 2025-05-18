c7c32ba881c4602e8b62894cd0d80cdb7fa9a7f7
Marwan Abdalla <marwan3m2000@gmail.com>
2025-05-19 00:46:45 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
+
+  const mockdata = [
+    'https://5sogwfu2yd.ufs.sh/f/vpz2ufVVQ9MSn3IiCRwK5l2PI71tLexQWowfB4bVYHRjOdZN',
+    'https://5sogwfu2yd.ufs.sh/f/vpz2ufVVQ9MShLcKRd6PaGdln0fkX2D81AI6j3cMQRypwvgZ',
+    'https://5sogwfu2yd.ufs.sh/f/vpz2ufVVQ9MST8Cs3tlVNktuiBmAwL1eFSPGz4Q8gH6x7Wpa',
+    'https://5sogwfu2yd.ufs.sh/f/vpz2ufVVQ9MSBNUKunhwerS6aJXLqCic3un5OmPEHjKYhT9o'
+  ]
+
+  const mockImages = mockdata.map((url, index) => ({
+    id: index + 1,
+    url
+  }))
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery
-    </main>
+    <div className="flex flex-wrap gap-4">
+      {mockImages.map((image) =>
+        <div key={image.id} className="w-48">
+          <img src={image.url} alt="a image"></img>
+        </div>)}
+    </div>
   );
 }
